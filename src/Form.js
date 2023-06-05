56b80b01da77c20001b85ee98af12c2ac8830e07
charlottemothersole <charlottemothersole@gmail.com>
2023-06-05 21:25:58 +0100

    Add robyn form

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,15 @@
 import './Form.css';
-import Image from './image.jpg';
+import Image1 from './img1.jpg';
+import Image2 from './img2.jpg';
 import React, { useState } from "react";
 
 export default function Form() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
+    const [robynName, robynSetName] = useState("");
+    const [robynEmail, robynSetEmail] = useState("");
+    const [robynMessage, robynSetMessage] = useState("");
     
     function handleNameChange(e) {
         setName(e.target.value)
@@ -19,29 +23,55 @@ export default function Form() {
         setMessage(e.target.value)
     }    
 
+    function handleRobynNameChange(e) {
+        robynSetName(e.target.value)
+    }    
+
+    function handleRobynEmailChange(e) {
+        robynSetEmail(e.target.value)
+    }    
+
+    function handleRobynMessageChange(e) {
+        robynSetMessage(e.target.value)
+    }    
+
     const handleSubmitCharlotte = (event) => {
+        // event.preventDefault();
+        
+    }
+
+    const handleSubmitRobyn = (event) => {
         // event.preventDefault();
         
     }
 
     return (
         <div id ='contact-section'>
-            <div id='form'>
-                <form  onSubmit={handleSubmitCharlotte}  method='POST' action='https://formspree.io/f/mjvdqgro'>            
-                    <input class="input-field" name='name' id='name' type='text' placeholder='Your Name:' value={name} onChange={handleNameChange}></input>
-                    <input class="input-field" name='email' id='email' type='email' placeholder='Your Email Address:' value={email} onChange={handleEmailChange}></input>
-                    <i class="fa fa-envelope icon"></i>
-                    <textarea class="input-field" name='message' id='message' type='text' placeholder='Your Message:' value={message} onChange={handleMessageChange}></textarea>
-                    <button id='submit-charlotte' type='submit'>Submit to Charlotte</button>
-                    {/* <button id='submit-robyn' type='button'>Submit to Robyn</button> */}
-                </form>
+            <div id='form-container-charlotte'>
+                <div id='form-charlotte'>
+                    <form id='charlotte' onSubmit={handleSubmitCharlotte}  method='POST' action='https://formspree.io/f/mjvdqgro'>            
+                        <input class="input-field" name='name' id='name' type='text' placeholder='Your Name:' value={name} onChange={handleNameChange}></input>
+                        <input class="input-field" name='email' id='email' type='email' placeholder='Your Email Address:' value={email} onChange={handleEmailChange}></input>
+                        <textarea class="input-field" name='message' id='message' type='text' placeholder='Your Message:' value={message} onChange={handleMessageChange}></textarea>
+                        <button id='submit-charlotte' type='submit'>Submit to Charlotte</button>
+                    </form>
+                {/* <h2>Current User Input: </h2>
+                <h4>{message}</h4>  */}
+                </div> 
+            </div>            
+            <img id='img-charlotte' height='400px' width='500px' src={Image1} alt='img text'/>             
+            <div id='form-container-robyn'>
+                <div id='form-robyn'>
+                    <form id='robyn' onSubmit={handleSubmitRobyn}  method='POST' action='https://formspree.io/f/mjvdqgro'>            
+                        <input class="input-field" name='robynName' id='robynName' type='text' placeholder='Your Name:' value={robynName} onChange={handleRobynNameChange}></input>
+                        <input class="input-field" name='robynEmail' id='robynEmail' type='email' placeholder='Your Email Address:' value={robynEmail} onChange={handleRobynEmailChange}></input>
+                        <textarea class="input-field" name='robynMessage' id='robynMessage' type='text' placeholder='Your Message:' value={robynMessage} onChange={handleRobynMessageChange}></textarea>
+                        <button id='submit-robyn' type='submit'>Submit to Robyn</button>
+                    </form>
+                </div> 
+            </div>
             
-{/*             
-            <h2>Current User Input: </h2>
-            <h4>{message}</h4> */}
-                <img height='300px' width='350px' src={Image} alt='img text'/>
-
-            </div>  
+            <img id='img-robyn' height='400px' width='500px' src={Image2} alt='img text'/> 
         </div>
               
     )
