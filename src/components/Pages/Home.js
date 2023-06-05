7cdac5127baf76321e9a5fa9cdc7de8b8d5a4cb4
Nybory <134627143+Nybory@users.noreply.github.com>
2023-06-05 09:29:40 +0100

    Update Home.js

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -10,15 +10,14 @@ const Home = () => {
             <Navbar/>
             <Banner/>
             <h2 className= 'HomeTitle'> Who Are We? </h2>
-            <h3 className="RobynTitle" > Robyn </h3>
-            <p className="RobynBio"> Hi, I'm Robyn! I am a student at CFG learning all about javascript, react and have created my very first website! 
+            <h3 className="NameTitle" > Robyn </h3>
+            <p className="NameBio"> Hi, I'm Robyn! I am a student at CFG learning all about javascript, react and have created my very first website! 
                 I have a background in fine art and I'm working hard to make the leap into the tech world. 
                 You can get in touch by checking out our contact us page!  </p>
-            <h4 className="CharTitle"> Charlotte </h4>
-            <p className="CharBio"> Hi, I'm Charlotte! I am a student at CFG learning all about javascript, react and have created my very first website! </p>
-            <h5> What Can We Do? </h5>
-            <p> We are skilled in the following: javascript, react, CSS, HTML, Github, Python, Figma.
-
+            <h4 className="NameTitle"> Charlotte </h4>
+            <p className="NameBio"> Hi, I'm Charlotte! I am a student at CFG learning all about javascript, react and have created my very first website! </p>
+            <h5 className="ActionTitle"> What Can We Do? </h5>
+            <p className="ActionBody"> We are skilled in the following: javascript, react, CSS, HTML, Github, Python, Figma.
             </p>
         </div>
     )
