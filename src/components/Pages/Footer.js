7ef780ca9e0b18b01ac26b3d65eb7fa595757042
charlottemothersole <charlottemothersole@gmail.com>
2023-06-06 17:58:28 +0100

    add footer styles

--- src/components/Pages/Footer.js
+++ src/components/Pages/Footer.js
@@ -4,7 +4,9 @@ import './Footer.css';
 export default function Footer() {
 
     return (
-        <footer>this is the footer</footer>
+        <footer>
+            this is the footer. It will have links to social media including github and linkedin
+        </footer>
     )
 
 }
