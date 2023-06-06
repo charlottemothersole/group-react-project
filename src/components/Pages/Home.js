cdecc2d3ef6dc68c8e53a81a2a25f8870eacc9e6
Nybory <134627143+Nybory@users.noreply.github.com>
2023-06-06 18:25:19 +0100

    Updated Home.js bios

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -21,8 +21,9 @@ const Home = () => {
                <div class="HomeContainer">
                <img src={RobImage} alt='img text' class='image'/>
                <div class="HomeOverlay">
-               <p className="NameBio"> Hi, I'm Robyn! I am a student at CFG learning all about javascript, react and have created my very first website! 
-                I have a background in fine art and I'm working hard to make the leap into the tech world. 
+               <p className="NameBio"> Hi, I'm Robyn! I have a background in Fine Art having studied Sculpture and Environmental Art. For the 
+               last year and a half i've been working at a small tech for good start up and have reccent got a new role as a service designer. 
+               Through my late role i've loved having the opportunity to learn python, CSS, HTML, Javascript, React and UX/UI design skills.
                 You can get in touch by checking out our contact us page!  </p>
                </div>
                </div>
@@ -30,7 +31,9 @@ const Home = () => {
                   <div class="HomeContainer">    
                   <img src={CharImage} alt='img text' class='image'/>
                   <div class="HomeOverlay">
-                  <p className="NameBio"> Hi, I'm Charlotte! I am a student at CFG learning all about javascript, react and have created my very first website! </p>
+                  <p className="NameBio"> Hi, I'm Charlotte! After working in Accountancy and Law, I completely changed direction and
+                   started pursuing software development. I have had some experience in HTML, CSS, Javascript, PHP, Python, Ruby and (some) React. I am currently 
+                   on a scholarship place in the Makers bootcamp and hope to find a role in the industry shortly afterwards!</p>
                   </div>
                   </div>
                   </div>
